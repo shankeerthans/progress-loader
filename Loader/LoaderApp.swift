e6cf153593deab9975ab90b4fef5448e51a39888
Shankeerthan <shankeerthan@4axissolutions.com>
2024-10-18 09:57:16 +0530

    Initial Commit

---
+++ Loader/LoaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoaderApp.swift
+//  Loader
+//
+//  Created by 4Axis on 2024-10-18.
+//
+
+import SwiftUI
+
+@main
+struct LoaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
